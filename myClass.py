989e10dcaec6fb6742c00e995a72b13c77b56862
Abhinandan Kumar <56464922+iam-abhijha@users.noreply.github.com>
2021-01-15 13:05:50 +0530

    login_done

---
+++ myClass.py
@@ -0,0 +1,28 @@
+# import webdriver
+from selenium import webdriver
+import time
+driver = webdriver.Firefox()
+
+driver.get("https://myclass.lpu.in/")
+
+user_id = driver.find_element_by_name("i")
+user_pass = driver.find_element_by_name("p")
+
+user_id.send_keys("11903648")
+user_pass.send_keys("Abhi1@")
+user_pass.submit()
+
+driver.implicitly_wait(10)
+view_class = driver.find_element_by_xpath("//a[contains(text(),'View Classes/Meetings')]")
+view_class.click()
+time.sleep(20)
+
+driver.implicitly_wait(5)
+try:
+    join = driver.find_element_by_xpath("//a[@class='btn btn-primary btn-block btn-sm']")
+    time.sleep(100)
+    print("Class Started")
+
+
+except:
+    print("Class not started, try after some time")
